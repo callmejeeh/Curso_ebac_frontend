6aee2c8a38a20ac4efd4cdcac2a676bf983a2dfe
Jess <138735984+callmejeeh@users.noreply.github.com>
2023-11-22 11:24:04 -0300

    added files

---
+++ tarefa.js
@@ -0,0 +1,21 @@
+const estudantes = [];
+const alunos = estudantes.map(function(nome, nota){
+    itemAtual= {
+        nome: nome,
+        nota: nota,
+    }
+    return itemAtual;
+})
+
+alunos.push({nome:'Lucio', nota: '5'});
+alunos.push({nome:'Ana', nota: '8'});
+alunos.push({nome:'João', nota: '10'});
+alunos.push({nome:'Renata', nota: '2'});
+alunos.push({nome:'Renata', nota: '4'});
+
+const alunosAprovados = alunos.filter(function(item){
+    return (item.nota >= '6');
+})
+console.log(alunosAprovados)
+
+// console.log(alunos)
