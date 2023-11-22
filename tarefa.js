4a590cf62a08eb2525cf270d0387acf89bcf8d38
Jess <138735984+callmejeeh@users.noreply.github.com>
2023-11-22 11:39:22 -0300

    Update tarefa.js

--- tarefa.js
+++ tarefa.js
@@ -7,11 +7,11 @@ const alunos = estudantes.map(function(nome, nota){
     return itemAtual;
 })
 
-alunos.push({nome:'Lucio', nota: '5'});
-alunos.push({nome:'Ana', nota: '8'});
-alunos.push({nome:'João', nota: '10'});
-alunos.push({nome:'Renata', nota: '2'});
-alunos.push({nome:'Ulisses', nota: '4'});
+alunos.push({nome:'Lucio', nota: 5});
+alunos.push({nome:'Ana', nota: 8});
+alunos.push({nome:'João', nota: 10});
+alunos.push({nome:'Renata', nota: 2});
+alunos.push({nome:'Ulisses', nota: 4});
 
 const alunosAprovados = alunos.filter(function(item){
     return (item.nota >= '6');
