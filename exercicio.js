65c5747ecb44f50a4e37298e238d200f7630a865
Jess <138735984+callmejeeh@users.noreply.github.com>
2023-11-23 08:59:41 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function calcula(a, b) {
+    return a * b;
+}
+calcula(10, 4);
+console.log(calcula);
+function dizOi(nome) {
+    return `${nome} diz oi`;
+}
+dizOi('Antônio');
+console.log(dizOi);
---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+function calcula(a:number, b:number):number{
+    return a * b;
+}
+
+calcula(10,4);
+console.log(calcula);
+
+function dizOi(nome:string):string{
+    return `${nome} diz oi`
+}
+
+dizOi('Antônio');
+console.log(dizOi);
